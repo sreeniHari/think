7bac7e2243db0dde61815194f583e19a96fbd5ba
Matthew Prestifilippo <mattp@aweber.com>
2018-02-16 19:40:24 -0500

    get working quiz

--- Exercises/quiz-app/assets/js/questions.js
+++ Exercises/quiz-app/assets/js/questions.js
@@ -7,7 +7,7 @@ var questions = [
             'New York',
             'California'
         ],
-        answer: 'Washington D.C.'
+        answer: 0
     },
     {
         text: 'Which pop duo was the first western band to play in The Peoples Republic of China?',
@@ -17,7 +17,107 @@ var questions = [
             'Chas and Dave',
             'Right Said Fred'
         ],
-        answer: 'Wham'
+        answer: 0
+    },
+    {
+        text: 'What is the capital of America?',
+        choices: [
+            'Washington D.C.',
+            'Texas',
+            'New York',
+            'California'
+        ],
+        answer: 0
+    },
+    {
+        text: 'Which pop duo was the first western band to play in The Peoples Republic of China?',
+        choices: [
+            'Wham',
+            'Simon and Garfunkel',
+            'Chas and Dave',
+            'Right Said Fred'
+        ],
+        answer: 0
+    },
+    {
+        text: 'What is the capital of America?',
+        choices: [
+            'Washington D.C.',
+            'Texas',
+            'New York',
+            'California'
+        ],
+        answer: 0
+    },
+    {
+        text: 'Which pop duo was the first western band to play in The Peoples Republic of China?',
+        choices: [
+            'Wham',
+            'Simon and Garfunkel',
+            'Chas and Dave',
+            'Right Said Fred'
+        ],
+        answer: 0
+    },
+    {
+        text: 'What is the capital of America?',
+        choices: [
+            'Washington D.C.',
+            'Texas',
+            'New York',
+            'California'
+        ],
+        answer: 0
+    },
+    {
+        text: 'Which pop duo was the first western band to play in The Peoples Republic of China?',
+        choices: [
+            'Wham',
+            'Simon and Garfunkel',
+            'Chas and Dave',
+            'Right Said Fred'
+        ],
+        answer: 0
+    },
+    {
+        text: 'What is the capital of America?',
+        choices: [
+            'Washington D.C.',
+            'Texas',
+            'New York',
+            'California'
+        ],
+        answer: 0
+    },
+    {
+        text: 'Which pop duo was the first western band to play in The Peoples Republic of China?',
+        choices: [
+            'Wham',
+            'Simon and Garfunkel',
+            'Chas and Dave',
+            'Right Said Fred'
+        ],
+        answer: 0
+    },
+    {
+        text: 'What is the capital of America?',
+        choices: [
+            'Washington D.C.',
+            'Texas',
+            'New York',
+            'California'
+        ],
+        answer: 0
+    },
+    {
+        text: 'Which pop duo was the first western band to play in The Peoples Republic of China?',
+        choices: [
+            'Wham',
+            'Simon and Garfunkel',
+            'Chas and Dave',
+            'Right Said Fred'
+        ],
+        answer: 0
     },
     // more questions...
 ]
