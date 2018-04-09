66639a8be0a0876392ede0fe655d0dde07a59e6a
sreenihari <HARI0030@e.ntu.edu.sg>
2018-04-10 07:11:12 +0800

    Building blocks

--- Exercises/capstoneFinancePortfolio/app.js
+++ Exercises/capstoneFinancePortfolio/app.js
@@ -1,19 +1,40 @@
-const STORE = [
-  {name: "AAPL"},
-];
+//FANG stocks to begin with
+//'FB','AAPL','AMZN','GOOG'}
+let STORE = [];
 
+function validateTickerSymbol(tickerSymbol) {
+  // Validate with yahoo finance API csv or NASDAQ csv
+}
 
 function generateTimeline(tickerSymbol, tickerIndex, apikey) {
   // Function to generate portfolio timeline
   //
 }
 
-function addtoPortfolio(tickerSymbol) {
-  STORE.push({name: tickerSymbol});
+function renderPortfolio() {
+  if (STORE.length > 0) {
+    const html = STORE.map(ticker => {
+      return '<li class="ticker">${ticker}</li>';
+    }).join('');
+    $('.portfolio').html(html);
+  }
 }
 
+function addtoPortfolio(tickerSymbol) {
+
+  // Push into store
+  STORE.push(tickerSymbol);
+
+  // Update local storage
+  localStorage.setItem('portfolio', JSON.stringify(STORE));
+
+  //render portfolio symbols
+  renderPortfolio();
+}
+
+
 function submitPortfolio() {
-  $('#js-portfolio-form').submit(function(event) {
+  $('#portfolio-form').submit(function(event) {
     event.preventDefault();
     const ticker = $('.js-tickerSymbol').val();
     $('.js-tickerSymbol').val('');
@@ -22,8 +43,16 @@ function submitPortfolio() {
 }
 
 
-function deletefromPortfolio() {
-  });
+function removefromPortfolio() {
+
+}
+
+
+function initialize() {
+  previousRetainedData = localStorage.getItem('portfolio');
+  if (previousRetainedData != null) {
+    STORE = JSON.parse(previousRetainedData);
+  }
 }
 
 // Functions to handle
@@ -31,8 +60,11 @@ function deletefromPortfolio() {
 // function to retrieve data using alphavantage API and input symbol
 // fuction to show timeline scale after symbol is added.
 function initPortfolioList() {
+  initialize();
   submitPortfolio();
-  deletefromPortfolio();
+  removefromPortfolio();
+  addtoPortfolio();
+  renderPortfolio();
 }
 
 // First method which is called when page is loaded
